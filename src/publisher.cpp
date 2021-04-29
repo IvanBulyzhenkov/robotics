e558bcbbd3d32b37fa17dc862e85f02b8b557f90
Ivan <43088248+IvanBulyzhenkov@users.noreply.github.com>
2021-04-29 16:46:34 +0700

    Add code of program
    * CMakeLists.txt - add dependencies
    * first_quadrant.png - plot for taget (80, 100)
    * fourth_quadrant.png - plot for target (80, -100)
    * plot.dat - file for drawing plot via gnuplot
    * plot.txt - file with coordinates
    * second_quadrant.png - plot for target (-80, 100)
    * src/publisher.cpp - code of model
    * src/subscriber.cpp - code of controller
    * third_quadrant.png - plot for target (-80, -100)

---
+++ src/publisher.cpp
@@ -0,0 +1,69 @@
+#include <fstream>
+#include <iostream>
+
+#include "ros/ros.h"
+#include "std_msgs/Float32.h"
+
+double v = 0;
+double w = 0;
+
+void vCallback(const std_msgs::Float32 data) {
+  v = data.data;
+}
+
+void wCallback(const std_msgs::Float32 data) {
+  w = data.data;
+}
+
+int main(int argc, char **argv) {
+  std::ofstream plot_file;
+  const std::string home = getenv("HOME");
+  plot_file.open((home + "/catkin_ws/src/robotics/plot.txt").c_str());
+
+  ros::init(argc, argv, "talker");
+  
+  ros::NodeHandle n;
+
+  ros::Publisher x_pub = n.advertise<std_msgs::Float32>("x_pub", 1000);
+  ros::Publisher y_pub = n.advertise<std_msgs::Float32>("y_pub", 1000);
+  ros::Publisher phi_pub = n.advertise<std_msgs::Float32>("phi_pub", 1000);
+
+  ros::Subscriber v_sub = n.subscribe("v_pub", 1000, vCallback);
+  ros::Subscriber w_sub = n.subscribe("w_pub", 1000, wCallback);
+
+  ros::Rate loop_rate(100);
+
+  double x_cur = 0.0;
+  double y_cur = 0.0;
+  double phi_cur = 0.0;
+  
+  double prev_time = ros::Time::now().toSec();
+
+  while (ros::ok()) {
+    std_msgs::Float32 x;
+    std_msgs::Float32 y;
+    std_msgs::Float32 phi;
+    
+    x_cur = x_cur + v * sin(phi_cur) * (ros::Time::now().toSec() - prev_time);
+    y_cur = y_cur + v * cos(phi_cur) * (ros::Time::now().toSec() - prev_time);
+    phi_cur = phi_cur + w;
+
+    x.data = x_cur;
+    y.data = y_cur;
+    phi.data = phi_cur;    
+
+    plot_file<<x.data<<" "<<y.data<<"\n";
+  
+    x_pub.publish(x);
+    y_pub.publish(y);
+    phi_pub.publish(phi);
+
+    ros::spinOnce();
+
+    prev_time = ros::Time::now().toSec();
+    loop_rate.sleep();
+  }
+
+
+  return 0;
+}
---
+++ src/subscriber.cpp
@@ -0,0 +1,90 @@
+#include "ros/ros.h"
+#include "std_msgs/Float32.h"
+
+double x;
+double y;
+double phi;
+
+void xCallback(const std_msgs::Float32 data) {
+  x = data.data;
+  ROS_INFO("x = %f", data.data);     
+}
+
+void yCallback(const std_msgs::Float32 data) {
+  y = data.data;
+  ROS_INFO("y = %f", data.data); 
+}
+
+void phiCallback(const std_msgs::Float32 data) {
+  phi = data.data;
+  ROS_INFO("phi = %f", data.data); 
+}
+
+int main(int argc, char **argv) {
+  double k_v = 0.5;
+  double k_w = 0.5;
+
+  double x_ref = 80;
+  double y_ref = -100;
+  double phi_ref = atan(x_ref / y_ref);
+
+  double v_cur;
+  double w_cur;
+  
+  std_msgs::Float32 v;
+  std_msgs::Float32 w;
+
+  ros::init(argc, argv, "listener");
+
+  ros::NodeHandle n;
+
+  ros::Subscriber x_sub = n.subscribe("x_pub", 1000, xCallback);
+  ros::Subscriber y_sub = n.subscribe("y_pub", 1000, yCallback);
+  ros::Subscriber phi_sub = n.subscribe("phi_pub", 1000, phiCallback);
+
+  ros::Publisher v_pub = n.advertise<std_msgs::Float32>("v_pub", 1000);
+  ros::Publisher w_pub = n.advertise<std_msgs::Float32>("w_pub", 1000);
+  
+
+  ros::Rate loop_rate(100);
+
+  double tolerance = 0.000001;
+
+  while (ros::ok) {
+     
+    if (abs(x - x_ref) < tolerance && abs(y - y_ref) < tolerance) {
+      std::cout<<"Goal reached"<<std::endl;
+      break;
+    }
+
+    v_cur = -k_v * ((x - x_ref) * sin(phi) + (y - y_ref) * cos(phi));
+    if (v_cur > 0.5) {
+      v_cur = 0.5;
+    }
+    if (v_cur < -0.5) {
+      v_cur = -0.5;
+    }
+
+    v.data = v_cur;
+
+    phi_ref = atan((x - x_ref) / (y - y_ref));
+    w_cur = -k_w * (phi - phi_ref);
+    if (w_cur > 0.01) {
+      w_cur = 0.01;
+    }
+    if (w_cur < -0.01) {
+      w_cur = -0.01;
+    }
+
+    w.data = w_cur;
+ 
+    v_pub.publish(v);
+    w_pub.publish(w);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+  }
+  
+  return 0;
+}
